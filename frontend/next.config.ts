fc83074fd094f33cb770ab330991fa149d4437f9
Saddam Hussain <saddamhmalik@gmail.com>
2026-05-17 21:11:33 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -27,6 +27,7 @@ const nextConfig: NextConfig = {
     ];
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com", pathname: "/**" },
       {
